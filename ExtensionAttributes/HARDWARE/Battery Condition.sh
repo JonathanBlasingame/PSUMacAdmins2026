0911309a606c18dc74cbd1ec796413f5d1603c14
Jonathan Blasingame <jonathan@blasingame.org>
2026-07-05 16:49:23 -0500

    Add script to report battery condition for laptops
    This script checks the battery condition of a laptop and reports it. It differentiates between ARM and Intel architectures.

---
+++ ExtensionAttributes/HARDWARE/Battery Condition.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+# EA-Description: Reports back the battery condition
+# EA-DataType: STRING
+
+arch=$(/usr/bin/arch)
+model=$(system_profiler SPHardwareDataType | grep "Model Name:" | cut -d ' ' -f 9)
+
+if [[ ! "$model" =~ "Book" ]]; then
+    echo "<result>Not A Laptop</result>"
+    return 0
+fi
+
+if [[ "$arch" == "arm64" ]]; then
+    capacity=$(system_profiler SPPowerDataType | grep "Maximum Capacity:" | sed 's/.*Maximum Capacity: //')
+    echo "<result>$(system_profiler SPPowerDataType | grep "Condition:" | sed 's/.*Condition: //') ($capacity)</result>"
+else
+    echo "<result>$(system_profiler SPPowerDataType | grep "Condition:" | sed 's/.*Condition: //')</result>"
+fi
