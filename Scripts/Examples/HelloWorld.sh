2d4f558e31e92330d9197b59a9e7336de90bf8fc
Jonathan Blasingame <jonathan@blasingame.org>
2026-07-05 16:46:57 -0500

    Add HelloWorld.sh example script for Jamf

---
+++ Scripts/Examples/HelloWorld.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Example Jamf script.
+# The filename (hello-world.sh) becomes the script name in Jamf Pro.
+# The parent folder (Examples) maps to the Jamf category.
+
+echo "Hello from Jamf as Code"
